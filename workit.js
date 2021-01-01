01c88d06321355e3ab47d5915150f8387792c1f9
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-01 16:53:27 -0300

    Update workit.js

--- workit.js
+++ workit.js
@@ -1,26 +1,21 @@
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
+dataJ = '';
+
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
       }
-    };
-    xhr.send();
-};
+    }
+  };
+  httpRequest.open('GET', path, false);
+  httpRequest.send();
+}
 
-getJSON('./data.json',
-function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    alert(data[0].Nome);
-    rafa = data[0].Nome;
-  }
+fetchJSONFile('data.json', function(data){
+  dataJ = data;
+  console.log(dataJ);
 });
-
-rafa = data[0].Nome
+console.log(dataJ);
