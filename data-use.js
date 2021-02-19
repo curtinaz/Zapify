01e09c63b74f671565a617439fddd344ff34a314
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-19 12:26:47 -0300

    did

--- data-use.js
+++ data-use.js
@@ -13,13 +13,14 @@ fetch('./data.json')
   /* FUNCIONAMENTO DA API */
 
      var userID = 0;
-//   var baseURL = `https://api.whatsapp.com/send?phone=`;
-//   var userDDD = dados.[userID].DDD;
+  var baseURL = `https://api.whatsapp.com/send?phone=`;
+  var userDDD = dados.[userID].DDD;
 
 function startThis() {
 
     document.querySelector("#userName").innerHTML = dados[userID].Nome
-    
+    document.querySelector("#whats").href = finalURL;
+
 }
 
 // https://api.whatsapp.com/send?phone=5551996842817&text=Boa%20tarde%2C%20Me%20chamo%20Fernanda%2C%20da%20Cin%C3%A9tica%20Edu!
