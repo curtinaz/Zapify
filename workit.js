6383e43390599d563d5e6a7b038dcee7e91ec3bf
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-02-19 12:01:17 -0300

    Update workit.js

--- workit.js
+++ workit.js
@@ -1,32 +1,32 @@
 //var csv is the CSV file with headers
 
-csv = document.querySelector("#csv")
+// csv = document.querySelector("#csv")
 
-var txtFile = new XMLHttpRequest();
-txtFile.open("GET", csv.value, true);
-txtFile.onreadystatechange = function()
-{
-    allText = txtFile.responseText;
-    allTextLines = allText.split(/\r\n|\n/);
-};
+// var txtFile = new XMLHttpRequest();
+// txtFile.open("GET", csv.value, true);
+// txtFile.onreadystatechange = function()
+// {
+//     allText = txtFile.responseText;
+//     allTextLines = allText.split(/\r\n|\n/);
+// };
 
 
 
-function csvJSON(txtFile) {
-    const lines = txtFile.split('\n')
-    const result = []
-    const headers = lines[0].split(',')
+// function csvJSON(txtFile) {
+//     const lines = txtFile.split('\n')
+//     const result = []
+//     const headers = lines[0].split(',')
 
-    for (let i = 1; i < lines.length; i++) {        
-        if (!lines[i])
-            continue
-        const obj = {}
-        const currentline = lines[i].split(',')
+//     for (let i = 1; i < lines.length; i++) {        
+//         if (!lines[i])
+//             continue
+//         const obj = {}
+//         const currentline = lines[i].split(',')
 
-        for (let j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j]
-        }
-        result.push(obj)
-    }
-    return result
-}
+//         for (let j = 0; j < headers.length; j++) {
+//             obj[headers[j]] = currentline[j]
+//         }
+//         result.push(obj)
+//     }
+//     return result
+// }
